395f6365b5b1d405c4f591eacde0a895514c8ce7
Sinayra <nayra@hotmail.co.jp>
2020-09-25 13:39:44 -0300

    Modelated data

---
+++ src/services/mock.tsx
@@ -0,0 +1,115 @@
+const mock = [
+    {
+        name: "Coca 1L",
+        unit: "UN",
+        purchase: [
+            {
+                date: new Date("2020-09-05"),
+                price: 11.18,
+                quantity: 2
+            },
+            {
+                date: new Date("2020-09-13"),
+                price: 10.38,
+                quantity: 2
+            },
+            {
+                date: new Date("2020-09-14"),
+                price: 10.38,
+                quantity: 2
+            },
+            {
+                date: new Date("2020-09-21"),
+                price: 11.18,
+                quantity: 2
+            }
+        ]
+    },
+    {
+        name: "Coca Zero 1.5L",
+        unit: "UN",
+        purchase: [
+            {
+                date: new Date("2020-09-05"),
+                price: 7.78,
+                quantity: 2
+            },
+            {
+                date: new Date("2020-09-21"),
+                price: 3.89,
+                quantity: 1
+            }
+        ]
+    },
+    {
+        name: "Chá",
+        unit: "UN",
+        purchase: [
+            {
+                date: new Date("2020-09-05"),
+                price: 8.99,
+                quantity: 1
+            },
+            {
+                date: new Date("2020-09-21"),
+                price: 17.98,
+                quantity: 2
+            }
+        ]
+    },
+    {
+        name: "Sabonete",
+        unit: "UN",
+        purchase: [
+            {
+                date: new Date("2020-09-05"),
+                price: 1.34,
+                quantity: 1
+            }
+        ]
+    },
+    {
+        name: "Batata",
+        unit: "KG",
+        purchase: [
+            {
+                date: new Date("2020-07-09"),
+                price: 2.77,
+                quantity: 0.5
+            },
+            {
+                date: new Date("2020-07-25"),
+                price: 1.40,
+                quantity: 0.28
+            },
+            {
+                date: new Date("2020-09-14"),
+                price: 0.87,
+                quantity: 0.29
+            }
+        ]
+    },
+    {
+        name: "Cenoura",
+        unit: "KG",
+        purchase: [
+            {
+                date: new Date("2020-07-25"),
+                price: 1.00,
+                quantity: 0.20
+            },
+            {
+                date: new Date("2020-07-28"),
+                price: 0.81,
+                quantity: 0.29
+            },
+            {
+                date: new Date("2020-09-14"),
+                price: 0.74,
+                quantity: 0.15
+            },
+        ]
+    }
+]
+
+export default mock;
