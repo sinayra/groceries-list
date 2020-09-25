d5ecc3ef3e8de6c86541f62b58107e860823ee4c
Sinayra <nayra@hotmail.co.jp>
2020-09-25 13:47:18 -0300

    Refactored pages

---
+++ src/pages/Grocery/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+export default function Grocery() {
+  return (
+    <View style={styles.container}>
+      <Text>Grocery page</Text>
+    </View>
+  );
+}
