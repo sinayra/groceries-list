37ea7f1478a99e63dff4f7c652addcbd690b4789
Sinayra <nayra@hotmail.co.jp>
2020-09-28 10:48:15 -0300

    Changed date to milliseconds

--- src/services/mock.tsx
+++ src/services/mock.tsx
@@ -3,22 +3,22 @@ const mock = [
         name: "Coca 1L",
         purchases: [
             {
-                date: new Date("2020-09-05"),
+                date: new Date("2020-09-05").getTime(),
                 price: 11.18,
                 quantity: 2
             },
             {
-                date: new Date("2020-09-13"),
+                date: new Date("2020-09-13").getTime(),
                 price: 10.38,
                 quantity: 2
             },
             {
-                date: new Date("2020-09-14"),
+                date: new Date("2020-09-14").getTime(),
                 price: 10.38,
                 quantity: 2
             },
             {
-                date: new Date("2020-09-21"),
+                date: new Date("2020-09-21").getTime(),
                 price: 11.18,
                 quantity: 2
             }
@@ -28,12 +28,12 @@ const mock = [
         name: "Coca Zero 1.5L",
         purchases: [
             {
-                date: new Date("2020-09-05"),
+                date: new Date("2020-09-05").getTime(),
                 price: 7.78,
                 quantity: 2
             },
             {
-                date: new Date("2020-09-21"),
+                date: new Date("2020-09-21").getTime(),
                 price: 3.89,
                 quantity: 1
             }
@@ -43,12 +43,12 @@ const mock = [
         name: "Chá",
         purchases: [
             {
-                date: new Date("2020-09-05"),
+                date: new Date("2020-09-05").getTime(),
                 price: 8.99,
                 quantity: 1
             },
             {
-                date: new Date("2020-09-21"),
+                date: new Date("2020-09-21").getTime(),
                 price: 17.98,
                 quantity: 2
             }
@@ -58,7 +58,7 @@ const mock = [
         name: "Sabonete",
         purchases: [
             {
-                date: new Date("2020-09-05"),
+                date: new Date("2020-09-05").getTime(),
                 price: 1.34,
                 quantity: 1
             }
@@ -68,22 +68,22 @@ const mock = [
         name: "Batata",
         purchases: [
             {
-                date: new Date("2020-07-09"),
+                date: new Date("2020-07-09").getTime(),
                 price: 2.77,
                 quantity: 0.55511022044088176352705410821643
             },
             {
-                date: new Date("2020-07-25"),
+                date: new Date("2020-07-25").getTime(),
                 price: 1.40,
                 quantity: 0.28056112224448897795591182364729
             },
             {
-                date: new Date("2020-09-14"),
+                date: new Date("2020-09-14").getTime(),
                 price: 0.87,
                 quantity: 0.29096989966555183946488294314381
             },
             {
-                date: new Date("2020-09-26"),
+                date: new Date("2020-09-26").getTime(),
                 price: 1.32,
                 quantity: 0.440
             }
@@ -93,17 +93,17 @@ const mock = [
         name: "Cenoura",
         purchases: [
             {
-                date: new Date("2020-07-25"),
+                date: new Date("2020-07-25").getTime(),
                 price: 1.00,
                 quantity: 0.20040080160320641282565130260521
             },
             {
-                date: new Date("2020-07-28"),
+                date: new Date("2020-07-28").getTime(),
                 price: 0.81,
                 quantity: 0.29032258064516129032258064516129
             },
             {
-                date: new Date("2020-09-14"),
+                date: new Date("2020-09-14").getTime(),
                 price: 0.74,
                 quantity: 0.1544885177453027139874739039666
             },
