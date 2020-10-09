a207c7f72ba8d5210151a4c64ec7b023dbbccfc1
Sinayra <nayra@hotmail.co.jp>
2020-10-09 10:32:45 -0300

    Refactored data mock
    Is now more similar to firebase database

--- src/services/mock.tsx
+++ src/services/mock.tsx
@@ -1,171 +1,149 @@
-const mock = [
-  {
-    "id": "1",
-    "name": "Coca 1L",
-    "purchases": [
-      {
-        "date": 1599264000000,
-        "price": 11.18,
-        "quantity": 2
-      },
-      {
-        "date": 1599955200000,
-        "price": 10.38,
-        "quantity": 2
-      },
-      {
-        "date": 1600041600000,
-        "price": 10.38,
-        "quantity": 2
-      },
-      {
-        "date": 1600646400000,
-        "price": 11.18,
-        "quantity": 2
+const mock = {
+  "groceries" : {
+    "0" : {
+      "name" : "Coca 1L",
+      "purchases" : [ {
+        "date" : 1599264000000,
+        "price" : 11.18,
+        "quantity" : 2
+      }, {
+        "date" : 1599955200000,
+        "price" : 10.38,
+        "quantity" : 2
+      }, {
+        "date" : 1600041600000,
+        "price" : 10.38,
+        "quantity" : 2
+      }, {
+        "date" : 1600646400000,
+        "price" : 11.18,
+        "quantity" : 2
+      } ]
+    },
+    "1" : {
+      "name" : "Coca Zero 1.5L",
+      "purchases" : [ {
+        "date" : 1599264000000,
+        "price" : 7.78,
+        "quantity" : 2
+      }, {
+        "date" : 1600646400000,
+        "price" : 3.89,
+        "quantity" : 1
+      } ]
+    },
+    "2" : {
+      "name" : "Chá",
+      "purchases" : [ {
+        "date" : 1599264000000,
+        "price" : 8.99,
+        "quantity" : 1
+      }, {
+        "date" : 1600646400000,
+        "price" : 17.98,
+        "quantity" : 2
+      } ]
+    },
+    "3" : {
+      "name" : "Sabonete",
+      "purchases" : [ {
+        "date" : 1599264000000,
+        "price" : 1.34,
+        "quantity" : 1
+      } ]
+    },
+    "4" : {
+      "name" : "Batata",
+      "purchases" : [ {
+        "date" : 1594252800000,
+        "price" : 2.77,
+        "quantity" : 0.5551102204408818
+      }, {
+        "date" : 1595635200000,
+        "price" : 1.4,
+        "quantity" : 0.280561122244489
+      }, {
+        "date" : 1600041600000,
+        "price" : 0.87,
+        "quantity" : 0.2909698996655518
+      }, {
+        "date" : 1601078400000,
+        "price" : 1.32,
+        "quantity" : 0.44
+      } ]
+    },
+    "5" : {
+      "name" : "Cenoura",
+      "purchases" : [ {
+        "date" : 1595635200000,
+        "price" : 1.0,
+        "quantity" : 0.20040080160320642
+      }, {
+        "date" : 1595894400000,
+        "price" : 0.81,
+        "quantity" : 0.2903225806451613
+      }, {
+        "date" : 1600041600000,
+        "price" : 0.74,
+        "quantity" : 0.1544885177453027
+      } ]
+    },
+    "6" : {
+      "name" : "Pão de forma",
+      "purchases" : [ {
+        "date" : 1599264000000,
+        "price" : 3.99,
+        "quantity" : 1
+      }, {
+        "date" : 1600646400000,
+        "price" : 3.19,
+        "quantity" : 1
+      }, {
+        "date" : 1598313600000,
+        "price" : 6.07,
+        "quantity" : 1
+      }, {
+        "date" : 1593734400000,
+        "price" : 4.39,
+        "quantity" : 1
+      }, {
+        "date" : 1595116800000,
+        "price" : 3.4,
+        "quantity" : 1
+      }, {
+        "date" : 1591142400000,
+        "price" : 4.19,
+        "quantity" : 1
+      }, {
+        "date" : 1591401600000,
+        "price" : 4.29,
+        "quantity" : 1
+      }, {
+        "date" : 1592092800000,
+        "price" : 4.79,
+        "quantity" : 1
+      }, {
+        "date" : 1593043200000,
+        "price" : 7.19,
+        "quantity" : 1
+      } ]
+    },
+    "-MJ7uyt7ZxyrE_XkrdQo" : {
+      "name" : "mais um produto",
+      "purchases" : {
+        "-MJ7uyu6vHZ-hgRS8Lna" : {
+          "date" : 1602172468142,
+          "price" : 3,
+          "quantity" : 3
+        },
+        "-MJ7v2o4EbJaTO90QG-T" : {
+          "date" : 1602172468142,
+          "price" : 2,
+          "quantity" : 2
+        }
       }
-    ]
-  },
-  {
-    "id": "2",
-    "name": "Coca Zero 1.5L",
-    "purchases": [
-      {
-        "date": 1599264000000,
-        "price": 7.78,
-        "quantity": 2
-      },
-      {
-        "date": 1600646400000,
-        "price": 3.89,
-        "quantity": 1
-      }
-    ]
-  },
-  {
-    "id": "3",
-    "name": "Chá",
-    "purchases": [
-      {
-        "date": 1599264000000,
-        "price": 8.99,
-        "quantity": 1
-      },
-      {
-        "date": 1600646400000,
-        "price": 17.98,
-        "quantity": 2
-      }
-    ]
-  },
-  {
-    "id": "4",
-    "name": "Sabonete",
-    "purchases": [
-      {
-        "date": 1599264000000,
-        "price": 1.34,
-        "quantity": 1
-      }
-    ]
-  },
-  {
-    "id": "5",
-    "name": "Batata",
-    "purchases": [
-      {
-        "date": 1594252800000,
-        "price": 2.77,
-        "quantity": 0.55511022044088176352705410821643
-      },
-      {
-        "date": 1595635200000,
-        "price": 1.4,
-        "quantity": 0.28056112224448897795591182364729
-      },
-      {
-        "date": 1600041600000,
-        "price": 0.87,
-        "quantity": 0.29096989966555183946488294314381
-      },
-      {
-        "date": 1601078400000,
-        "price": 1.32,
-        "quantity": 0.44
-      }
-    ]
-  },
-  {
-    "id": "6",
-    "name": "Cenoura",
-    "purchases": [
-      {
-        "date": 1595635200000,
-        "price": 1.0,
-        "quantity": 0.20040080160320641282565130260521
-      },
-      {
-        "date": 1595894400000,
-        "price": 0.81,
-        "quantity": 0.29032258064516129032258064516129
-      },
-      {
-        "date": 1600041600000,
-        "price": 0.74,
-        "quantity": 0.1544885177453027139874739039666
-      }
-    ]
-  },
-  {
-    "id": "7",
-    "name": "Pão de forma",
-    "purchases": [
-      {
-        "date": 1599264000000,
-        "price": 3.99,
-        "quantity": 1
-      },
-      {
-        "date": 1600646400000,
-        "price": 3.19,
-        "quantity": 1
-      },
-      {
-        "date": 1598313600000,
-        "price": 6.07,
-        "quantity": 1
-      },
-      {
-        "date": 1593734400000,
-        "price": 4.39,
-        "quantity": 1
-      },
-      {
-        "date": 1595116800000,
-        "price": 3.4,
-        "quantity": 1
-      },
-      {
-        "date": 1591142400000,
-        "price": 4.19,
-        "quantity": 1
-      },
-      {
-        "date": 1591401600000,
-        "price": 4.29,
-        "quantity": 1
-      },
-      {
-        "date": 1592092800000,
-        "price": 4.79,
-        "quantity": 1
-      },
-      {
-        "date": 1593043200000,
-        "price": 7.19,
-        "quantity": 1
-      }
-    ]
+    }
   }
-];
+}
 
 export default mock;
