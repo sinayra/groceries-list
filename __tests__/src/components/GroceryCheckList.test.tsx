2982fdd44003270ccdda20990b8fc99a282272d0
sinayra <nayra@hotmail.co.jp>
2020-11-24 12:01:04 +0100

    Test if elements from purchase list are not rendered

--- __tests__/src/components/GroceryCheckList.test.tsx
+++ __tests__/src/components/GroceryCheckList.test.tsx
@@ -130,8 +130,6 @@ describe('<Grocery Check List />', () => {
                 const showList: Grocery[] = [
                     {
                         name: "Sabonete",
-                        listId: "0",
-                        listQuantity: 1,
                         purchases: [{
                             "date": 1599264000000,
                             "price": 1.34,
@@ -139,8 +137,6 @@ describe('<Grocery Check List />', () => {
                         }]
                     },
                     {
-                        listId: "1",
-                        listQuantity: 3,
                         name: "Batata",
                         purchases: [{
                             "date": 1594252800000,
@@ -161,8 +157,6 @@ describe('<Grocery Check List />', () => {
                         }]
                     },
                     {
-                        listId: "2",
-                        listQuantity: 1,
                         name: "Chá",
                         purchases: [{
                             "date": 1599264000000,
@@ -185,8 +179,6 @@ describe('<Grocery Check List />', () => {
             it('one element', () => {
                 const showList: Grocery[] = [{
                     name: "Sabonete",
-                    listId: "0",
-                    listQuantity: 1,
                     purchases: [{
                         "date": 1599264000000,
                         "price": 1.34,
@@ -205,8 +197,187 @@ describe('<Grocery Check List />', () => {
 
                 const component = shallow(<GroceryCheckList showList={showList} canBeAddedToList={addToList} reload={reload} />);
                 const list = component.find({ testID: "list" });
+                const text = component.find(Text).getElement().props.children;
 
                 expect(list).toHaveLength(0);
+                expect(text).toBe("Não há itens na sua Lista.");
+            });
+
+            describe('has items in purchase list', () => {
+                it('has elements to render', () => {
+                    const showList: Grocery[] = [
+                        {
+                            id:"1",
+                            name: "Sabonete",
+                            listId: "0",
+                            listQuantity: 1,
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 1.34,
+                                "quantity": 1
+                            }]
+                        },
+                        {
+                            id: "2",
+                            name: "Batata",
+                            purchases: [{
+                                "date": 1594252800000,
+                                "price": 2.77,
+                                "quantity": 0.5551102204408818
+                            }, {
+                                "date": 1595635200000,
+                                "price": 1.4,
+                                "quantity": 0.280561122244489
+                            }, {
+                                "date": 1600041600000,
+                                "price": 0.87,
+                                "quantity": 0.2909698996655518
+                            }, {
+                                "date": 1601078400000,
+                                "price": 1.32,
+                                "quantity": 0.44
+                            }]
+                        },
+                        {
+                            id: "3",
+                            name: "Chá",
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 8.99,
+                                "quantity": 1
+                            }, {
+                                "date": 1600646400000,
+                                "price": 17.98,
+                                "quantity": 2
+                            }]
+                        }
+                    ];
+
+                    const purchaseList: Grocery[] = [
+                        {
+                            id: "1",
+                            name: "Sabonete",
+                            listId: "0",
+                            listQuantity: 1,
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 1.34,
+                                "quantity": 1
+                            }]
+                        }
+                    ];
+
+                    const component = shallow(<GroceryCheckList showList={showList} canBeAddedToList={addToList} reload={reload} purchaseList={purchaseList} />);
+                    const list = component.find({ testID: "list" });
+
+                    expect(list).toHaveLength(1);
+                });
+
+                it('does not have elements to render', () => {
+                    const showList: Grocery[] = [
+                        {
+                            id:"1",
+                            name: "Sabonete",
+                            listId: "0",
+                            listQuantity: 1,
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 1.34,
+                                "quantity": 1
+                            }]
+                        },
+                        {
+                            id: "2",
+                            name: "Batata",
+                            purchases: [{
+                                "date": 1594252800000,
+                                "price": 2.77,
+                                "quantity": 0.5551102204408818
+                            }, {
+                                "date": 1595635200000,
+                                "price": 1.4,
+                                "quantity": 0.280561122244489
+                            }, {
+                                "date": 1600041600000,
+                                "price": 0.87,
+                                "quantity": 0.2909698996655518
+                            }, {
+                                "date": 1601078400000,
+                                "price": 1.32,
+                                "quantity": 0.44
+                            }]
+                        },
+                        {
+                            id: "3",
+                            name: "Chá",
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 8.99,
+                                "quantity": 1
+                            }, {
+                                "date": 1600646400000,
+                                "price": 17.98,
+                                "quantity": 2
+                            }]
+                        }
+                    ];
+
+                    const purchaseList: Grocery[] = [
+                        {
+                            id:"1",
+                            name: "Sabonete",
+                            listId: "0",
+                            listQuantity: 1,
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 1.34,
+                                "quantity": 1
+                            }]
+                        },
+                        {
+                            id: "2",
+                            name: "Batata",
+                            purchases: [{
+                                "date": 1594252800000,
+                                "price": 2.77,
+                                "quantity": 0.5551102204408818
+                            }, {
+                                "date": 1595635200000,
+                                "price": 1.4,
+                                "quantity": 0.280561122244489
+                            }, {
+                                "date": 1600041600000,
+                                "price": 0.87,
+                                "quantity": 0.2909698996655518
+                            }, {
+                                "date": 1601078400000,
+                                "price": 1.32,
+                                "quantity": 0.44
+                            }]
+                        },
+                        {
+                            id: "3",
+                            name: "Chá",
+                            purchases: [{
+                                "date": 1599264000000,
+                                "price": 8.99,
+                                "quantity": 1
+                            }, {
+                                "date": 1600646400000,
+                                "price": 17.98,
+                                "quantity": 2
+                            }]
+                        }
+                    ];
+
+                    const component = shallow(<GroceryCheckList showList={showList} canBeAddedToList={addToList} reload={reload} purchaseList={purchaseList} />);
+                    const list = component.find({ testID: "list" });
+                    const text = component.find(Text).getElement().props.children;
+
+                    expect(list).toHaveLength(0);
+                    expect(text).toBe("Não há itens na sua Lista.");
+                });
+
             });
         });
     });
